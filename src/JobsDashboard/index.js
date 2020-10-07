9818fa3545208c8d4211261d57f760a5e4a0bd10
Erik Carlson <carlsonerikw@gmail.com>
2020-10-07 08:29:20 -0400

    Fix brand logo. Add placeholders for filter select work

--- src/JobsDashboard/index.js
+++ src/JobsDashboard/index.js
@@ -56,11 +56,9 @@ function JobsDashboard() {
       <header>
         <div id="brand">
           <h1 id='logo'>
-            <span className='icon'>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
-              </svg>
-            </span>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+            </svg>
             jobs
           </h1>
         </div>
@@ -169,7 +167,12 @@ function JobsDashboard() {
         </div>
         <div id="page-content" >
           <div id='dashboard-container'>
-            <div className='card' id='dashboard-filter'>fitlter content here</div>
+            <div className='card' id='dashboard-filter'>
+              <div>All Jobs</div>
+              <div>Skills</div>
+              <div>Category</div>
+              <div>Date Range</div>
+            </div>
             {job_data.map(job => {
               const { id, label, location, createDate, candidates } = job;
               return (
