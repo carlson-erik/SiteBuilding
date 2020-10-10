1dcb6ee36ff55bfebeeb8bfa438c50054d260451
Erik Carlson <carlsonerikw@gmail.com>
2020-10-09 21:52:33 -0400

    Add route for job dashboard and create initial home page. Add focus styling to job dashboard

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 import JobsDashboard from './JobsDashboard';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Site Building Home Page</h1>
+      <p>There will be a cool home page with descriptions of each site I've built</p>
+    </div>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <JobsDashboard />
+    <Router>
+      <Switch>
+        <Route path='/job-dashboard'>
+          <JobsDashboard />
+        </Route>
+        <Route path='/'>
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
