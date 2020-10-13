c6062a6e03299ab8e8e0c3698fa0a558f819847e
Erik Carlson <carlsonerikw@gmail.com>
2020-10-12 22:04:56 -0400

    Move Table filter and most of job data to styled-components

--- src/jobs-dashboard/components/filter-table.js
+++ src/jobs-dashboard/components/filter-table.js
@@ -1,6 +1,125 @@
 import React from 'react';
 import Select from 'react-dropdown-select';
+import styled from 'styled-components';
+/* ------------ Styled Components ------------ */
+import { Link } from '../../global/styled-components/';
+const Card = styled.div`
+  background-color: white;
+  border: 1px solid #edeff3;
+  width: 100%;
+  border-radius: 0.5rem;
+  -webkit-box-shadow: 4px 4px 4px 0px rgba(0,0,0,0.1);
+  -moz-box-shadow: 4px 4px 4px 0px rgba(0,0,0,0.1);
+  box-shadow: 4px 4px 4px 0px rgba(0,0,0,0.1);
+  margin-bottom: 1rem;
+`;
 
+const DashboardFilter = styled(Card)`
+  margin-bottom: 2rem;
+  display: flex;
+  flex-direction: column;
+
+  & > *:last-child{
+    padding: 1rem 1rem 1rem 1rem;
+  }
+`;
+
+const FilterContainer = styled.div`
+  flex-grow: 1;
+  width: 100%;
+  display: flex;
+`;
+
+const ShowAllContainer = styled.div`
+  width: 100%;
+  display: flex;
+  padding: 0 0 1rem 1rem;
+  align-self: flex-end;
+
+  & .icon {
+    color: #9faabd;
+  } 
+`;
+
+const Filter = styled.div`
+  flex: 1 0 24%;
+  padding: 1rem 0 1rem 1rem;
+
+  &:last-child{
+    padding: 1rem 1rem 1rem 1rem;
+  }
+
+  & > div > div {
+    padding: 0.5rem;
+    border-radius: 0.5rem;
+    font-size: 16px;
+  }
+
+  & div > input {
+    font-size: 16px;
+  }
+`;
+
+const SelectItem = styled.div`
+  padding: 0.5rem;
+  color: #555;
+  border-radius: 3px;
+  cursor: pointer;
+  font-size: 16px;
+
+  * > div {
+    display: flex;
+    align-items: center;
+  }
+
+  & input {
+    margin-right: 10px;
+  }
+
+  &:hover {
+    background-color: #f4f9ff;
+    color: #1e86ff;
+  }
+`;
+
+const JobCard = styled(Card)`
+  display: flex;
+`;
+
+const JobDataContainer = styled.div`
+  display: flex;
+  flex-grow: 1;
+  padding: 1rem;
+`;
+
+const DataColumn = styled.div`
+  flex: 1 0 33%;
+  display: flex;
+  align-items: center;
+  color: #8f9cb2;
+
+  @media only screen and (max-width: 960px) {
+    text-align: center;
+  }
+`;
+
+const JobTitle = styled.div`
+  flex: 1 0 33%;
+  display: flex;
+  flex-direction: column;
+  align-items: normal;
+`;
+
+const JobLabel = styled.span`
+  font-weight: bold;
+  color: #303446;
+`;
+
+const JobLocation = styled.span`
+  color: #bac2d0;
+`;
+
+/* ------------ Render Data ------------ */
 const job_data = [
   {
     id: 'front-end-dev-1',
@@ -90,20 +209,21 @@ const options = [
   },
 ];
 
+
 function FilterTable() {
   return (
     <React.Fragment>
-      <div className='card' id='dashboard-filter'>
-        <div id='filter-container'>
-          <div className='filter'>
+      <DashboardFilter>
+        <FilterContainer>
+          <Filter>
             <Select
               placeholder='All Jobs'
               options={[]}
               values={[]}
               onChange={(value) => console.log(value)}
             />
-          </div>
-          <div className='filter'>
+          </Filter>
+          <Filter>
             <Select
               multi
               placeholder='Skills'
@@ -112,7 +232,7 @@ function FilterTable() {
               onChange={(value) => console.log(value)}
               itemRenderer={({ item, methods }) => {
                 return (
-                  <div className='select-item'>
+                  <SelectItem>
                     {item.disabled ? (
                       <div aria-disabled>{item.label}</div>
                     ) : (
@@ -120,56 +240,55 @@ function FilterTable() {
                           <input onChange={() => methods.addItem(item)} type="checkbox" checked={methods.isSelected(item)} /> {item.label}
                         </div>
                       )}
-                  </div>
+                  </SelectItem>
                 );
               }}
             />
-          </div>
-          <div className='filter'>
+          </Filter>
+          <Filter>
             <Select
               placeholder='Category'
               options={[]}
               values={[]}
               onChange={(value) => console.log(value)}
             />
-          </div>
-          <div className='filter'>
+          </Filter>
+          <Filter>
             <Select
               placeholder='Date Range'
               options={[]}
               values={[]}
               onChange={(value) => console.log(value)}
             />
-          </div>
-
-        </div>
-        <div id='show-all-filters'>
-          <a href="#" className='link'>
+          </Filter>
+        </FilterContainer>
+        <ShowAllContainer>
+          <Link>
             Show all filters
                   <span className='icon'>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
               </svg>
             </span>
-          </a>
-        </div>
-      </div>
+          </Link>
+        </ShowAllContainer>
+      </DashboardFilter>
       {job_data.map(job => {
         const { id, label, location, createDate, candidates } = job;
         return (
-          <div key={id} className='card job'>
-            <div className='job-data'>
-              <div className='data-column job-title'>
-                <span className='job-label'>{label}</span>
-                <span className='job-location'>{location}</span>
-              </div>
-              <div className='data-column'>
+          <JobCard key={id}>
+            <JobDataContainer>
+              <JobTitle>
+                <JobLabel>{label}</JobLabel>
+                <JobLocation>{location}</JobLocation>
+              </JobTitle>
+              <DataColumn>
                 <span>{createDate}</span>
-              </div>
-              <div className='data-column'>
+              </DataColumn>
+              <DataColumn>
                 <span>{candidates} Candidates</span>
-              </div>
-            </div>
+              </DataColumn>
+            </JobDataContainer>
             <div className='actions'>
               <a href='#' className='link-button primary'>
                 <button tabIndex="-1">
@@ -191,7 +310,7 @@ function FilterTable() {
                 </button>
               </a>
             </div>
-          </div>
+          </JobCard>
         )
       })}
 
