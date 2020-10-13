e293f0d4bc6790a1fa369493c78c1baafc5a03e0
Erik Carlson <carlsonerikw@gmail.com>
2020-10-12 21:25:27 -0400

    Move search and pager panel components to styled-components.

--- src/jobs-dashboard/components/search.js
+++ src/jobs-dashboard/components/search.js
@@ -1,16 +1,50 @@
 import React from 'react';
+import styled from 'styled-components';
 /* ------------ Styled Components ------------ */
+const SearchBar = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 3rem;
+  background-color: white;
+  border: 1px solid #e7eaee;
+  border-radius: 0.5rem;
+
+  & .icon {
+    padding-left: 1rem;
+    color: #9faabd;
+  }
+
+  & .icon > * {
+    height: 20px;
+  }
+`;
+
+const SearchInput = styled.input`
+  line-height: 1.5;
+  width: 100%;
+  border: none;
+  color: #a7b0bc;
+  padding: 0.5rem;
+  
+  &:focus {
+    outline: none;
+  }
+
+  @media only screen and (max-width: 1175px) {
+    width: 300px;
+  }
+`;
 
 function Search() {
   return (
-    <div id='search-bar'>
+    <SearchBar>
       <span className='icon'>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
         </svg>
       </span>
-      <input type="text" placeholder="Search for jobs, candidates,.." />
-    </div>
+      <SearchInput type="text" placeholder="Search for jobs, candidates,.." />
+    </SearchBar>
   );
 };
 
